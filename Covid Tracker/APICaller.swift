b8b6e8a63a1aeb1a4169d6e86b461f339d8cb750
mwyzer <muhammad.wyzer@gmail.com>
2022-01-02 10:54:45 +0700

    first commit

---
+++ Covid Tracker/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  Covid Tracker
+//
+//  Created by Muhammad Wyzer on 27/12/21.
+//
+
+import Foundation
---
+++ Covid Tracker/FilterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FilterViewController.swift
+//  Covid Tracker
+//
+//  Created by Muhammad Wyzer on 27/12/21.
+//
+
+import SwiftUI
+
+struct FilterViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterViewController()
+    }
+}
